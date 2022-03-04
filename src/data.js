5402638546cf85cbe3315380151882a9feb3a595
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-03-04 19:47:45 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,104 @@
+export const sliderItems = [
+    {
+      "title": "Codeless Data Structures and Algorithms",
+      "subtitle": "Learn DSA Without Writing a Single Line of Code",
+      "isbn13": "9781484257241",
+      "price": "$26.71",
+      "image": "https://itbook.store/img/books/9781484257241.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "Data Structures and Algorithms",
+      "subtitle": "Annotated Reference with Examples",
+      "isbn13": "1001622115743",
+      "price": "$0.00",
+      "image": "https://itbook.store/img/books/1001622115743.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "Microsoft Dynamics 365 For Dummies",
+      "subtitle": "Accelerate your digital transformation and break down silos with Microsoft Dynamics 365",
+      "isbn13": "9781119508861",
+      "price": "$19.39",
+      "image": "https://itbook.store/img/books/9781119508861.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    }
+  ];
+
+  export const categories = [
+   {
+      "title": "Multithreaded JavaScript",
+      "subtitle": "Concurrency Beyond the Event Loop",
+      "isbn13": "9781098104436",
+      "price": "$43.89",
+      "image": "https://itbook.store/img/books/9781098104436.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "PHP: The Right Way",
+      "subtitle": "Your guide to PHP best practices, coding standards, and authoritative tutorials",
+      "isbn13": "1001635859424",
+      "price": "$0.00",
+      "image": "https://itbook.store/img/books/1001635859424.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+        {
+      "title": "Beginning Python",
+      "subtitle": "Using Python 2.6 and Python 3.1",
+      "isbn13": "9780470414637",
+      "price": "$7.99",
+      "image": "https://itbook.store/img/books/9780470414637.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "Python Cookbook, 2nd Edition",
+      "subtitle": "Recipes from the Python Community",
+      "isbn13": "9780596007973",
+      "price": "$7.17",
+      "image": "https://itbook.store/img/books/9780596007973.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+  ];
+
+  export const popularProducts = [
+       {
+      "title": "Coffee Break Python Slicing",
+      "subtitle": "24 Workouts to Master Slicing in Python, Once and for All",
+      "isbn13": "1001605784161",
+      "price": "$0.00",
+      "image": "https://itbook.store/img/books/1001605784161.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "Learn More Python 3 the Hard Way",
+      "subtitle": "The Next Step for New Python Programmers",
+      "isbn13": "9780134123486",
+      "price": "$22.33",
+      "image": "https://itbook.store/img/books/9780134123486.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "Programming in Python 3, 2nd Edition",
+      "subtitle": "A Complete Introduction to the Python Language",
+      "isbn13": "9780321680563",
+      "price": "$33.54",
+      "image": "https://itbook.store/img/books/9780321680563.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "Beginning Python",
+      "subtitle": "Using Python 2.6 and Python 3.1",
+      "isbn13": "9780470414637",
+      "price": "$7.99",
+      "image": "https://itbook.store/img/books/9780470414637.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+    {
+      "title": "Python Cookbook, 2nd Edition",
+      "subtitle": "Recipes from the Python Community",
+      "isbn13": "9780596007973",
+      "price": "$7.17",
+      "image": "https://itbook.store/img/books/9780596007973.png",
+      "url": "https://itbook.store/files/9781617294136/chapter2.pdf"
+    },
+  ]
